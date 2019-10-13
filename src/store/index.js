d4ee80e7bbcb240927931f02b9b5d333dcbfc843
Gabriel Pessanha <gabrielcmp1@gmail.com>
2019-10-13 17:20:11 -0300

    initial commit, vue-cli project with vuexstore

---
+++ src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import Firebase from 'firebase';
+
+Vue.use(Vuex);
+
+
+export const store = new Vuex.Store({
+  state: {
+
+  },
+  mutations: {
+
+  },
+  getters: {
+
+  },
+  actions: {
+
+  }
+});
