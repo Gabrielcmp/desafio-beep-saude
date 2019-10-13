464ecea638930a72c4fe2ad5810d66e941a0b865
Gabriel Pessanha <gabrielcmp1@gmail.com>
2019-10-13 18:54:08 -0300

    get first story comments

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -29,10 +29,16 @@
     </ul>
     <button @click="updateStoriesIDs">update stories</button>
     <p>{{ topstoriesIDs }}</p>
-    <button @click="updateStoriesObjects">update stories Objects</button>
+    <button @click="updateTop15Stories">update stories Objects</button>
     <li v-for="story in top15Stories">
       {{ story }}
     </li>
+    <br>
+    <button @click="updateFirstStoryComments">update comments</button>
+    <li v-for="comment in topStoryComments">
+      {{ comment }}
+    </li>
+
   </div>
 </template>
 
@@ -50,7 +56,10 @@ export default {
       return this.$store.getters.getTopstoriesIDs
     },
     top15Stories() {
-      return this.$store.getters.top15Stories
+      return this.$store.getters.getTop15Stories
+    },
+    topStoryComments() {
+      return this.$store.getters.getTopStoryComments
     }
   },
   methods: {
@@ -60,7 +69,9 @@ export default {
     updateTop15Stories() {
       this.$store.dispatch('updateTop15StoriesAction')
     },
-
+    updateFirstStoryComments() {
+      this.$store.dispatch('updateFirstStoryComments')
+    }
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { isEmpty } from "lodash"
 // import Firebase from 'firebase';
 
 // const ref = new Firebase("https://hacker-news.firebaseio.com/v0/");
@@ -12,18 +13,23 @@ export const store = new Vuex.Store({
   state: {
     topstoriesIDs: [],
     top15Stories: [],
+    topStoryComments: []
   },
   mutations: {
     SET_TOPSTORIES_IDS(state, stories) {
-      state.topstories = stories
+      state.topstoriesIDs = stories
     },
     SET_TOP15_STORIES(state, storyObjects) {
-      state.topstoriesObjects = storyObjects
+      state.top15Stories = storyObjects
+    },
+    SET_STORY_COMMENTS(state, comments) {
+      state.topStoryComments = comments
     }
   },
   getters: {
     getTopstoriesIDs: state => state.topstoriesIDs,
-    getTop15Stories: state => state.top15Stories
+    getTop15Stories: state => state.top15Stories,
+    getTopStoryComments: state => state.topStoryComments
   },
   actions: {
     updateTopstoriesIDsAction({ commit }) {
@@ -42,6 +48,25 @@ export const store = new Vuex.Store({
       })).then(storyObjects => {
         commit('SET_TOP15_STORIES', storyObjects)
       })
-    }
+    },
+    updateFirstStoryComments({ commit, getters }) {
+      let commentIDs = getters.getTop15Stories[0].kids;
+      if (!isEmpty(commentIDs)) {
+        Promise.all(commentIDs.map(commentId => {
+          return new Promise(resolve => {
+            fetch(`${baseHNUrl}/item/${commentId}.json`).then(r => r.json()).then(comment => {
+              resolve(comment);
+            })
+          })
+        })).then(comments => {
+          commit(
+            'SET_STORY_COMMENTS',
+            comments.filter(comment => {
+              return comment.text && comment.text.length > 20
+            })
+          )
+        })
+      }
+  }
   }
 });
